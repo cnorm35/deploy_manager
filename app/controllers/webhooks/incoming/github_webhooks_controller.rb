da91f22defc729c2f51b9ccd80737ae1edb206d7
Cody Norman <wcodynorman@gmail.com>
2023-04-12 10:52:07 -0600

    kick off deploy on github webhook event

--- app/controllers/webhooks/incoming/github_webhooks_controller.rb
+++ app/controllers/webhooks/incoming/github_webhooks_controller.rb
@@ -1,6 +1,21 @@
 class Webhooks::Incoming::GithubWebhooksController < Webhooks::Incoming::WebhooksController
   def create
-    Webhooks::Incoming::GithubWebhook.create(data: JSON.parse(request.body.read)).process_async
+    Webhooks::Incoming::GithubWebhook.create(data: parsed_body).process_async
+    app_id = Rails.application.credentials.dig(:hatchbox, :app_id)
+    hatchbox_deploy_url = "https://app.hatchbox.io/webhooks/deployments/#{app_id}?latest=true"
+    deployment_id = HTTParty.post(hatchbox_deploy_url)
+    puts "deployment_id: #{deployment_id}***************"
+
+    check_url = "https://app.hatchbox.io/apps/#{app_id}/activities/#{deployment_id}"
+    deploy_status = HTTParty.get(check_url)
+    # send post request
+    # after create hook to deploy changes?
     render json: {status: "OK"}, status: :created
+  end
+
+  private
+
+  def parsed_body
+    JSON.parse(request.body.read)
   end
 end
